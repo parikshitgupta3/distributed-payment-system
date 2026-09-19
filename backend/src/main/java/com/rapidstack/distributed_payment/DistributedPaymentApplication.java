8b37f30b9670ca1baa60b3969fe0b65ab38a1b22
Parikshit <parikshitgupta2001@gmail.com>
2026-09-19 23:37:35 +0530

    init

---
+++ backend/src/main/java/com/rapidstack/distributed_payment/DistributedPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.rapidstack.distributed_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedPaymentApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rapidstack/distributed_payment/DistributedPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapidstack.distributed_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
